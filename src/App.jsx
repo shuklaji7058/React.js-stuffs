8e26532191deacccd883805192b2f2c0c906f9ff
Anish Shukla <shuklaanish040@gmail.com>
2024-10-16 18:38:39 +0530

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
-export default function App() {
+import React, { useState } from "react";
+
+const App = () => {
+  const [username, setUsername] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(username);
+    setUsername("");
+  };
   return (
-    <h1 className="text-3xl font-bold underline bg-red-500">Hello world!</h1>
+    <div>
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+      >
+        <input
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+          className="px-3 py-4 text-xl m-5 rounded  bg-blue-50 outline-blue-300"
+          type="text"
+          placeholder="Enter Your name"
+        ></input>
+        <button className="px-6 py-3 ml-9 text-xl  rounded   bg-emerald-700 font-semibold">
+          Submit
+        </button>
+      </form>
+    </div>
   );
-}
+};
+export default App;
