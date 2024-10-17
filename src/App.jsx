c24edcf979944d8255d18685314461c2c4a0fc0d
Anish Shukla <shuklaanish040@gmail.com>
2024-10-17 19:51:12 +0530

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,30 @@
-import React from "react";
-import Card from "./components/Card";
+import axios from "axios";
+import React, { useState } from "react";
 
 const App = () => {
-  const users = [
-    {
-      name: "Alice Johnson",
-      city: "New York",
-      age: 28,
-      profession: "Software Engineer",
-      profile_photo: "https://example.com/photos/alice.jpg",
-    },
-    {
-      name: "Michael Brown",
-      city: "Los Angeles",
-      age: 35,
-      profession: "Graphic Designer",
-      profile_photo: "https://example.com/photos/michael.jpg",
-    },
-    {
-      name: "Sophia Lee",
-      city: "Chicago",
-      age: 24,
-      profession: "Marketing Specialist",
-      profile_photo: "https://example.com/photos/sophia.jpg",
-    },
-    {
-      name: "David Smith",
-      city: "San Francisco",
-      age: 42,
-      profession: "Product Manager",
-      profile_photo: "https://example.com/photos/david.jpg",
-    },
-    {
-      name: "Emma Davis",
-      city: "Houston",
-      age: 31,
-      profession: "Architect",
-      profile_photo: "https://example.com/photos/emma.jpg",
-    },
-  ];
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const response = await axios.get("https://picsum.photos/v2/list");
+
+    setData(response.data);
+    console.log(data[0].download_url);
+  };
+
   return (
-    <div>
-      <div className="p-10">
-        {users.map(function (elem, idx) {
-          return (
-            <Card
-              key={idx}
-              username={elem.name}
-              age={elem.age}
-              prof={elem.profession}
-              city={elem.city}
-              photo={elem.profile_photo}
-            />
-          );
-        })}
-      </div>
+    <div className="p-9">
+      <div className="p-5 mt-5 bg-gray-500"></div>
+      {data.map(function (elem, idx) {
+        return (
+          <div
+            key={idx}
+            className="bg-gray-100 text-black flex items-center justify-between py-6 px-7 rounded mb-3"
+          >
+            <img className="h-40" src={elem.download_url} alt=""></img>
+            <h1>{elem.author}</h1>
+          </div>
+        );
+      })}
     </div>
   );
 };
