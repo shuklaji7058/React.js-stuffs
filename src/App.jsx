74c2147fe10e7ddcd0703e2186de937127c66e8b
Anish Shukla <shuklaanish040@gmail.com>
2024-10-18 17:12:48 +0530

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
-import axios from "axios";
-import React, { useState } from "react";
+import React from "react";
 
 const App = () => {
-  const [data, setData] = useState([]);
-
-  const getData = async () => {
-    const response = await axios.get("https://picsum.photos/v2/list");
-
-    setData(response.data);
-    console.log(data[0].download_url);
-  };
-
-  return (
-    <div className="p-9">
-      <div className="p-5 mt-5 bg-gray-500"></div>
-      {data.map(function (elem, idx) {
-        return (
-          <div
-            key={idx}
-            className="bg-gray-100 text-black flex items-center justify-between py-6 px-7 rounded mb-3"
-          >
-            <img className="h-40" src={elem.download_url} alt=""></img>
-            <h1>{elem.author}</h1>
-          </div>
-        );
-      })}
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
