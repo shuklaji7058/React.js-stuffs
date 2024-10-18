679a5a4ce974ab1c3859cdcc2d9c45dccebabd28
Anish Shukla <shuklaanish040@gmail.com>
2024-10-18 18:40:50 +0530

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
