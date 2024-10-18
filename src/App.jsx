a8edf3159aac919511236cdaeb04d426a115c69b
Anish Shukla <shuklaanish040@gmail.com>
2024-10-18 19:21:32 +0530

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Home from "./pages/Home";
-import Product from "./pages/Product";
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/product" element={<Product />} />
-      </Routes>
+      <h1>This is APP</h1>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import UserContext from "./context/UserContext.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <UserContext>
     <App />
-  </BrowserRouter>
+  </UserContext>
 );
